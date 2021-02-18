027cb21ef0175fa84d9b6b707c08527fd4bdc3f8
piotr-may <walecznypiotr@gmail.com>
2021-02-18 19:58:20 +0100

    2 Zajęcia Update
    To co zrobiliśmy na 2 zajęciach

---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CameraController : MonoBehaviour
+{
+    public float mouseSensivity = 430f;
+
+    public Transform playerBody = null;
+
+    private float xRotation = 0f;
+
+    private void Awake()
+    {
+        playerBody = transform.parent;
+    }
+
+    void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+    }
+
+   
+    void Update()
+    {
+        float mouseX = Input.GetAxis("Mouse X") * mouseSensivity * Time.deltaTime;
+        float mouseY = Input.GetAxis("Mouse Y") * mouseSensivity * Time.deltaTime;
+
+        xRotation -= mouseY;
+        xRotation = Mathf.Clamp(xRotation, -90f, 80f);
+
+        transform.localRotation = Quaternion.Euler(xRotation, 0f,0f);
+        playerBody.Rotate(Vector3.up * mouseX);
+    }
+}
---
+++ Assets/Scripts/PlayerControler.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControler : MonoBehaviour
+{
+    public float speed = 12f;
+    public float gravity = -10f;
+
+
+    //sluzy do obliczenia spadania
+    private Vector3 velocity;
+    private CharacterController characterController;
+
+    private void Awake()
+    {
+        characterController = GetComponent<CharacterController>();
+    }
+
+    void Update()
+    {
+        float x = Input.GetAxis("Horizontal");
+        float z = Input.GetAxis("Vertical");
+
+        Vector3 move = transform.right * x + transform.forward * z;
+        characterController.Move(move * speed* Time.deltaTime);
+
+        velocity.y += gravity;
+        characterController.Move(velocity * Time.deltaTime);
+    }
+}
